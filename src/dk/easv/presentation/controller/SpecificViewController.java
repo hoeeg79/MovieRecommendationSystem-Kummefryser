653311db3a90573a31b5b1aadd2f2232c6326a2c
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-02-08 11:42:30 +0100

    pre peders magic touch

---
+++ src/dk/easv/presentation/controller/SpecificViewController.java
@@ -0,0 +1,9 @@
+package dk.easv.presentation.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class SpecificViewController {
+    @FXML
+    private ListView LView;
+}
