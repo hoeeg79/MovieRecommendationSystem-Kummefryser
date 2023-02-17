41d3d0e3b69c19e451dcfd04d926d0a597a2d271
DanKjaer <114471756+DanKjaer@users.noreply.github.com>
2023-02-17 11:42:10 +0100

    navneneVirker

--- src/dk/easv/presentation/controller/SelectMovieViewController.java
+++ src/dk/easv/presentation/controller/SelectMovieViewController.java
@@ -9,12 +9,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class SelectMovieViewController extends BaseController{
 
+    @FXML
+    private Label lblTitle;
     private AppModel model;
     @FXML
     private Button btnMovie;
@@ -23,6 +26,7 @@ public class SelectMovieViewController extends BaseController{
     public void setModel(AppModel model, Movie movie) {
         this.model = model;
         this.movie = movie;
+        lblTitle.textProperty().set(movie.getTitle() + " (" + movie.getYear() + ")");
     }
 
     public void handleMovieBtn(ActionEvent actionEvent) {
